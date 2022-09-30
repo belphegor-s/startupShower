f75126fc7014b69462f1c64d7f953eed1e8b3b14
Ayush Sharma <ayush2162020@gmail.com>
2022-09-30 14:28:44 +0530

    updated styles -- landing page

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,46 @@
 // Modal
-
-var modal = document.getElementById("signup");
+// var modal = document.getElementById("signup");
 var btn = document.getElementById("signup-btn");
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
   // modal.style.display = "block";
   window.location = "/form";
 };
 
-span.onclick = function () {
-  modal.style.display = "none";
-};
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
 
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
 
 // Form Data
-var registrationForm = document.getElementById("registration-form");
+// var registrationForm = document.getElementById("registration-form");
 
-registrationForm.onsubmit = (e) => {
-  e.preventDefault();
+// registrationForm.onsubmit = (e) => {
+//   e.preventDefault();
 
-  var personName = document.getElementById("personName").value;
-  var email = document.getElementById("email").value;
-  var contact = document.getElementById("contact").value;
-  var age = document.getElementById("age").value;
-  var state = document.getElementById("state").value;
-  var fundraisingHistory = document.getElementById("fundraising-history").value;
-  var businessIdea = document.getElementById("business-idea").value;
+//   var personName = document.getElementById("personName").value;
+//   var email = document.getElementById("email").value;
+//   var contact = document.getElementById("contact").value;
+//   var age = document.getElementById("age").value;
+//   var state = document.getElementById("state").value;
+//   var fundraisingHistory = document.getElementById("fundraising-history").value;
+//   var businessIdea = document.getElementById("business-idea").value;
 
-  const data = {
-    name: personName,
-    email: email,
-    contact: contact,
-    age: age,
-    state: state,
-    fundraisingHistory: fundraisingHistory,
-    businessIdea: businessIdea,
-  };
+//   const data = {
+//     name: personName,
+//     email: email,
+//     contact: contact,
+//     age: age,
+//     state: state,
+//     fundraisingHistory: fundraisingHistory,
+//     businessIdea: businessIdea,
+//   };
 
-  console.log(data);
-};
+//   console.log(data);
+// };
